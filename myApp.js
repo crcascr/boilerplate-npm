50a0a0d843ac7cd8e2b7cba6cc622433d374bb49
crcascr <cristianj_20@hotmail.com>
2025-03-29 17:35:16 -0500

    add several api endpoints

--- myApp.js
+++ myApp.js
@@ -1,7 +1,47 @@
+require('dotenv').config();
+require('body-parser')
 let express = require('express');
 let app = express();
 
+app.use(express.urlencoded({ extended: false }));
+
+app.use((req, res, next) => {
+    console.log(`${req.method} ${req.path} - ${req.ip}`);
+    next();
+})
+
+app.use('/public', express.static(__dirname + '/public'));
 console.log('Hello World')
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/views/index.html');
+})
+app.get('/json', (req, res) => {
+    let message = 'Hello json';
+
+    if (process.env.MESSAGE_STYLE === 'uppercase') {
+        message = message.toUpperCase();
+    }
+
+    res.json({ message });
+});
+app.get('/now', (req, res, next) => {    
+    req.time = new Date().toString();
+    next();
+}, (req, res) => {
+    res.json({ time: req.time });
+})
+app.get('/:word/echo', (req, res) => {    
+    res.json({ echo: req.params.word });
+})
+app.get('/name', (req, res) => {    
+    res.json({ name: `${req.query.first} ${req.query.last}` });
+})
+app.post('/name', (req, res) => {
+    let firstName = req.body.first;
+    let lastName = req.body.last;
+    res.json({ name: `${firstName} ${lastName}` });
+});
+
 
 
 
